b86d67d63e9fdb211698708eec7d32128433fee2
Vishal verma <vishal.verma3441@gmail.com>
2025-08-31 13:14:56 +0530

    testing with API

--- src/app.js
+++ src/app.js
@@ -5,6 +5,17 @@ const app =express();
 app.use("/test",(req,res)=>{
     res.send("heelo from the server");
 });
+
+app.get("/user",(req,res)=>{
+res.send("today i have used the get version of API")
+});
+
+app.post("/user",(req,res)=>{
+    // console.log("save data to database");
+    res.send("Data is send to the database successfully");
+    });
+
+
 app.listen(3000,()=>{
     console.log("server is successfully started");
 });
