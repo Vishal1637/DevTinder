33241720fc946611288374d011bdd3628df9ab1d
Vishal verma <vishal.verma3441@gmail.com>
2025-09-09 00:47:58 +0530

    schem

--- src/app.js
+++ src/app.js
@@ -2,19 +2,27 @@ const express = require('express');
 
 const app =express();
 
-app.use("/test",(req,res)=>{
-    res.send("heelo from the server");
-});
+const { adminAuth ,userAuth} = require("./middlewares/auth");
 
-app.get("/user",(req,res)=>{
-res.send("today i have used the get version of API")
-});
+//handle all the middleware for all Get,Post request...
+
+app.use("/admin",adminAuth);
 
 app.post("/user",(req,res)=>{
-    // console.log("save data to database");
-    res.send("Data is send to the database successfully");
-    });
+res.send("User is succesfully logged in to the app");
+});
 
+app.get("/user/data",userAuth,(req,res)=>{
+res.send("user data send");
+});
+app.get("/admin/getalldata", (req, res) => {
+    res.send("sends all the data");
+
+});
+
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("deleted a user");
+    });
 
 app.listen(3000,()=>{
     console.log("server is successfully started");
